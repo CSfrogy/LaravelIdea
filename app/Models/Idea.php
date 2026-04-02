a95dfea06493e8d6d62c203c3f222513d81feb11
CSfrogy <lonelyguse@gmail.com>
2026-04-02 20:05:42 +0800

    Enum set up

---
+++ app/Models/Idea.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\IdeaStatus;
+use Database\Factories\IdeaFactory;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Idea extends Model
+{
+    /** @use HasFactory<IdeaFactory> */
+    use HasFactory;
+
+    protected $casts
+        = [
+            'links' => AsArrayObject::class,
+            'status' => IdeaStatus::class,
+        ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
