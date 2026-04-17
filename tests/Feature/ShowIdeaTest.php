a2a6fad1b9432034f55feabcb6985e511f6b413f
CSfrogy <lonelyguse@gmail.com>
2026-04-17 15:48:22 +0800

    upload commit

--- tests/Feature/ShowIdeaTest.php
+++ tests/Feature/ShowIdeaTest.php
@@ -8,7 +8,7 @@ it('must be signed in to see an idea', function () {
     $this->get(route('idea.show', $idea))->assertRedirectToRoute('login');
 });
 
-it('disallws accessing an idea you did not create', function () {
+it('disallows accessing an idea you did not create', function () {
     $user = User::factory()->create();
     $this->actingAs($user);
     $idea = Idea::factory()->create();
